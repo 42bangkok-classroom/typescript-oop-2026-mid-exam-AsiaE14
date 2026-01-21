d122c277d087547b30483db45625f6bea10dd2e0
AsiaE14 <sadgudo@gmail.com>
2026-01-21 15:03:54 +0700

    fix

--- src/practicals/p02/grading.ts
+++ src/practicals/p02/grading.ts
@@ -8,21 +8,21 @@
 }else{
    let  args =Number(argv);
     if(args >= 80 && args <=100){
-        console.log('Grad is A');
+        console.log('Grade is A');
     
      }else if(args >= 70 && args <=79){
-        console.log('Grad is B');
+        console.log('Grade is B');
     
      }
      else if(args >= 60 && args <=69){
-        console.log('Grad is C');
+        console.log('Grade is C');
     
      }
      else if(args >= 50 && args <=59){
-        console.log('Grad is D');
+        console.log('Grade is D');
     
      }else if(args >= 0 && args <=49){
-        console.log('Grad is F');
+        console.log('Grade is F');
     
      }
     
