63cc1eb26ecd1922bdfa0c2b9e4ed76cb5be8798
AsiaE14 <sadgudo@gmail.com>
2026-01-21 15:07:47 +0700

    fix

--- src/practicals/p04/number-pattern.ts
+++ src/practicals/p04/number-pattern.ts
@@ -5,19 +5,22 @@ if(isNaN(Number(a))){
 
 
 }else{
-    let pattern =" "
+    let pattern =""
  let  args =Number(a);
 
- for (let i = 0; i <=args; i++) {    //first 'for loop' for horizontal lines
-    for (let j = 0; j <= (args-i); j++) {    //second 'for loop' to add 'spaces' before pattern
-        pattern += " ";
+ for (let i = 0; i <=args; i++) {    
+    for (let j = 0; j <= (args-i); j++) {  
+        
+            pattern += " ";
+     
+      
     }
 
-    for (let k = 0; k <= i; k++) {      //third 'for loop' to add * to pattern string
+    for (let k = 0; k <= i; k++) {     
         pattern += k+1;
     }
-    pattern += "\n";    //new line after row completion
+    pattern += "\n";  
 }
-console.log(pattern);   //printing star to console
+console.log(pattern);   
 }
 
